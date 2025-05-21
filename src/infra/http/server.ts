b15ecdb2b4c81828f32043e6b2a7f8d6a658093b
paulo-gouvea <paulogouvea1994@gmail.com>
2025-05-21 19:20:40 -0300

    feat: adding zod into fastify + created create-link

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,12 +1,33 @@
-import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { createLinkRoute } from './routes/create-link'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal Server Error.' })
+})
+
 server.register(fastifyCors, { origin: '*' })
 
-console.log(env)
+server.register(createLinkRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP SERVER RUNNING!')
