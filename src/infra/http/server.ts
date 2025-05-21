12f23213cb13fbf1c8aa66942dd9bd83dd929fc6
paulo-gouvea <paulogouvea1994@gmail.com>
2025-05-21 19:04:30 -0300

    feat: adding postgresql docker and zod

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,13 @@
-import { fastify } from "fastify";
-import { fastifyCors } from "@fastify/cors";
+import { env } from '@/env'
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
 
 const server = fastify()
 
 server.register(fastifyCors, { origin: '*' })
 
+console.log(env)
+
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-    console.log('HTTP SERVER RUNNING!')
+  console.log('HTTP SERVER RUNNING!')
 })
